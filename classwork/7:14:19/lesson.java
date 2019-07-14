bf0bc72e78a2c282b5cbf36a4f5dde81b9bb05a3
Ian Lu <ianlu@Ians-MacBook-Air.local>
2019-07-14 16:57:45 -0400

    lesson pushed

---
+++ classwork/7:14:19/lesson.java
@@ -0,0 +1,71 @@
+// Teletype
+
+public class lesson{
+  public static void main(String[] args){
+    System.out.print("Hello World");
+        System.out.print(5);
+        /*
+          THE TWO TYPES OF VARIABLES IN JAVA:
+          Primitive Types:
+            - integer (int)
+            - decimals (double)
+            - booleans
+            - characters
+            - ....
+
+          Object Types:
+            - Strings
+        */
+        // store an integer in a value
+        // create a variable named var and give it a value of 7
+        int var = 7;
+        System.out.println(var);
+        // one equal sign means assignment. the thing to its left is being assigned the value to its right.
+        // store a decimal in a value
+        // create a variable named dec and give it a value of 9.3
+        double dec = 9.3;
+        System.out.println(dec);
+        // one equal sign means assignment. the thing to its left is being assigned the value to its right.
+        boolean flag = true;
+        System.out.println(flag);
+
+        char character = 's';
+        System.out.println(character);
+
+        String str = "this is a test";
+        System.out.println(str);
+          // When calling or creating objects, we always have to capatlize the first character
+          double a = 10;
+          double b = 3.0;
+          double c = a / b;
+          System.out.println(c);
+
+          int d = 10;
+          int e = 4;
+          System.out.println(d%e);
+          //% means modular arithmetic. finds remainder when dividing d by e
+          String newstr = "this is a new string";
+          newstr += " this is an add on!";
+            // shorthand way of writing   newstr = newstr + " this is an add on!";
+          System.out.println(newstr);
+          //looping in JAVA
+          //two types of loops
+            //for loop
+            //while loop
+
+            // first part is the assignment
+            // second part is the condition
+            // third part is the increment
+            // i++ is same as i += 1 same as i = i + 1
+            for (int i = 0; i < 10; i++;){
+              System.out.println(j);
+            }
+  }
+}
+//notes:
+//end statements with semicolons;
+/*ln stands for new line
+
+
+
+*/
